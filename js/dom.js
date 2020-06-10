55cd4df67cb9c3729e59f397e83a129278187ef2
this-is-jassa <jassa.developer@gmail.com>
2020-06-10 01:51:07 -0400

    changes

--- js/dom.js
+++ js/dom.js
@@ -1,13 +1,13 @@
 export default class DOM {
 
+    AnimationTime = 1000;
 
     $ = (s, o = document) => o.querySelector(s);
-    AnimationTime = 5000;
     // $$ = (s, o = document) => o.querySelectorAll(s);
 
     steps = [];
 
-    $interval = new Rx.Observable.interval(5000);
+    $interval = new Rx.Observable.interval(this.AnimationTime);
     $dataflow;
 
     directionsIcon = {
@@ -23,6 +23,7 @@ export default class DOM {
 
     showSteps(steps = []) {
 
+
         try {
             this.steps = [...steps];
             this.$dataflow.unsubscribe();
@@ -31,16 +32,32 @@ export default class DOM {
         } catch (err) {
 
         } finally {
-            this.$dataflow = new Rx.BehaviorSubject([1, 2, 3]).switchMap(
-                steps => {
+            this.$dataflow = new Rx.BehaviorSubject(steps).switchMap(
+                () => {
                     return this.$interval
                 }
             ).subscribe(value => {
-                this.$('#navigate').style.animation = `navigate ${this.AnimationTime / 1000}s alternate-reverse infinite`;
-                this.$('#progressBar').style.animation = `progress ${this.AnimationTime / 1000}s alternate-reverse infinite`
-                this.$('#title').innerHTML =  this.steps[value].maneuver.instruction;
+
+
+                this.$('#navigate').classList.add('navigation_animation');
+                this.$('#navigate').style.animationDuration = (this.AnimationTime / 1000) + 's';
+
+                this.$('#progressBar').classList.add('progressBar_animation');
+                this.$('#progressBar').style.animationDuration = (this.AnimationTime / 1000) + 's';
+
+                this.$('#title').innerHTML = this.steps[value].maneuver.instruction;
                 this.$('#discription').innerHTML = this.steps[value].name;
                 this.$('#sign').innerHTML = this.directionsIcon[this.steps[value].maneuver.modifier];
+                
+                if (value === this.steps.length - 1) {
+
+                    console.log(this.$('#navigate').classList);
+                    this.$('#navigate').classList.remove('navigation_animation');
+                    this.$('#progressBar').classList.remove('progressBar_animation');
+
+                    this.$dataflow.unsubscribe();
+
+                }
 
             })
         }
